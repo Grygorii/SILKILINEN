0bc8258592d9237b1030ba8cbb7e2197742277a4
Grygorii <grisha.kinzerskyi@gmail.com>
2026-04-29 21:12:09 +0100

    backend server connected to MongoDB

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(function() {
+    console.log('Connected to MongoDB');
+  })
+  .catch(function(err) {
+    console.log('MongoDB connection error:', err);
+  });
+
+app.get('/', function(req, res) {
+  res.send('Silkilinen backend is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log('Server running on port ' + PORT);
+});
